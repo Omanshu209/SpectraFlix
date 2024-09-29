ff4a494d140c6287ccb8ad871b16a68458d789c2
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-09-29 14:23:46 +0530

    Update `Movie.java`

--- Movie.java
+++ Movie.java
@@ -2,18 +2,21 @@ package com.app.backend;
 
 public class Movie
 {
-	private String title, imagePath, titleImagePath, trailerId, wistiaMovieEmbedCode, gDriveMovieEmbedCode, localMoviePath;
-	private int popularity;
+	private String title, imagePath, titleImagePath, trailerId, wistiaMovieEmbedCode, gDriveMovieEmbedCode, localMoviePath, description;
+	private int popularity, resImagePath, resTitleImagePath;
 	
 	// Action, Adventure, Animation, Comedy, Crime, Documentary, Drama, Family, Fantasy, History, Horror, Music, Mystery, Romance, Science Fiction, TV movie, Thriller, War, Western
 	private int[] genres = new int[19];
 	
-	public Movie(String title, String imagePath, String titleImagePath, String trailerId, String wistiaMovieEmbedCode, String gDriveMovieEmbedCode, String localMoviePath, int popularity, int[] genres)
+	public Movie(String title, String imagePath, int resImagePath, int resTitleImagePath, String titleImagePath, String trailerId, String wistiaMovieEmbedCode, String gDriveMovieEmbedCode, String localMoviePath, int popularity, String description, int[] genres)
 	{
 		this.title = title;
 		this.imagePath = imagePath;
 		this.titleImagePath = titleImagePath;
 		this.trailerId = trailerId;
+		this.resImagePath = resImagePath;
+		this.resTitleImagePath = resTitleImagePath;
+		this.description = dewcription;
 		this.wistiaMovieEmbedCode = wistiaMovieEmbedCode;
 		this.gDriveMovieEmbedCode = gDriveMovieEmbedCode;
 		this.localMoviePath = localMoviePath;
@@ -34,6 +37,21 @@ public class Movie
 	public String getTitleImagePath()
 	{
 		return this.titleImagePath;
+	}
+	
+	public int getResImagePath()
+	{
+		return this.resImagePath;
+	}
+	
+	public int getResTitleImagePath()
+	{
+		return this.resTitleImagePath;
+	}
+	
+	public String description()
+	{
+		return this.description;
 	}
 	
 	public String getTrailerId()
@@ -74,6 +92,21 @@ public class Movie
 	public void setImagePath(String imagePath)
 	{
 		this.imagePath = imagePath;
+	}
+	
+	public void setResImagePath(int resImagePath)
+	{
+		this.resImagePath = resImagePath;
+	}
+	
+	public void setResTitleImagePath(int resTitleImagePath)
+	{
+		this.resTitleImagePath = resTitleImagePath;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
 	}
 	
 	public void setTitleImagePath(String titleImagePath)
