cf2a8dfb9992cb69bf069ea5102803db0d14439e
omanshu209 <omanshu209@gmail.com>
2024-10-07 00:50:02 +0530

    Added Movies

--- SpectraFlix/app/src/main/java/com/app/spectraflix/Movie.java
+++ SpectraFlix/app/src/main/java/com/app/spectraflix/Movie.java
@@ -5,12 +5,9 @@ import java.io.Serializable;
 public class Movie implements Serializable
 {
 	private String title, imagePath, titleImagePath, trailerId, movieEmbedCode, movieURL, localPath, description;
-	private int popularity, resImagePath, resTitleImagePath;
+	private int resImagePath, resTitleImagePath;
 	
-	// Action, Adventure, Animation, Comedy, Crime, Documentary, Drama, Family, Fantasy, History, Horror, Music, Mystery, Romance, Science Fiction, TV movie, Thriller, War, Western
-	private int[] genres = new int[19];
-	
-	public Movie(String title, String imagePath, String titleImagePath, int resImagePath, int resTitleImagePath, String trailerId, String movieEmbedCode, String movieURL, String localPath, int popularity, String description, int[] genres)
+	public Movie(String title, String imagePath, String titleImagePath, int resImagePath, int resTitleImagePath, String trailerId, String movieEmbedCode, String movieURL, String localPath, String description)
 	{
 		this.title = title;
 		this.imagePath = imagePath;
@@ -22,8 +19,6 @@ public class Movie implements Serializable
 		this.movieEmbedCode = movieEmbedCode;
 		this.movieURL = movieURL;
 		this.localPath = localPath;
-		this.popularity = popularity;
-		this.genres = genres;
 	}
 	
 	public String getTitle()
@@ -76,16 +71,6 @@ public class Movie implements Serializable
 		return this.movieURL;
 	}
 	
-	public int getPopularity()
-	{
-		return this.popularity;
-	}
-	
-	public int[] getGenres()
-	{
-		return this.genres;
-	}
-	
 	public void setTitle(String title)
 	{
 		this.title = title;
@@ -134,15 +119,5 @@ public class Movie implements Serializable
 	public void setLocalPath(String localPath)
 	{
 		this.localPath = localPath;
-	}
-	
-	public void setPopularity(int popularity)
-	{
-		this.popularity = popularity;
-	}
-	
-	public void setGenres(int[] genres)
-	{
-		this.genres = genres;
 	}
 }
